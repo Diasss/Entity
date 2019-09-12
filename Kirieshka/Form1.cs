72013c02719d8c13b5e35696d79e8843b81368cb
Диас Оспанов <@local.itstep>
2019-09-12 17:29:54 +0600

    Добавьте файлы проекта.

---
+++ Kirieshka/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kirieshka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyDataBaseEntities mydb = new MyDataBaseEntities();
+            dataGridView1.DataSource = mydb.Table.ToList();
+        }
+    }
+}
